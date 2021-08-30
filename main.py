03ea72b152843bae7bcb9eb23a9cc15dc56e9421
Lucas Roberts <89323713+LucasRoberts@users.noreply.github.com>
2021-08-30 17:09:33 -0400

    Add files via upload
    - Fixed price not updating error --> added User-Agent header
    - User can now specify a time interval for retrieving stock price (defaults to 10 mins)
    - Started specifying when program should run.

--- main.py
+++ main.py
@@ -1,65 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime as dt
+from datetime import date
+import time
 
-# Grabs user inputted stock ticker
 ticker = input("Please enter the ticker symbol of the stock you would like to see: ").upper()
+interval = input("How often do you want to retrieve the stock price (in minutes): ")
+try:
+    interval = int(interval)
+    if interval <= 0:
+        interval = (interval * -1) * 60
+    else:
+        interval = interval * 60
+except ValueError:
+    interval = 600
 
-# Yahoo's Stock URL link on Yahoo finance
 url = f"https://finance.yahoo.com/quote/{ticker}?p={ticker}&.tsrc=fin-srch"
 
-# using requests retrieves urls html docs
-r = requests.get(url)
+# One major reason the price hasn't been updating was because the website was checking the user-agent
+# Now that it thinks it's a browser it will return updated prices
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/72.0. 3626.121 Safari/537.36."}
 
-# converts html doc from string into bs4 usable text
-html = r.content
-soup = BeautifulSoup(html, "lxml")
-
-# Grabs the title of the page, for this it will include the stocks name and ticker
-# print(soup.title)
-
-# Again, there has to be an easier way I simply have not found that way yet
-# Due to website change. I updated the code to try and correct the change in html
-current_stock_price = soup.find("body").div.div.div.div.div.find("div", class_="YDC-Lead").div.div.find("div", id="Lead-4-QuoteHeader-Proxy").div.find("div", class_="My(6px) Pos(r) smartphone_Mt(6px)").div.find("span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").text
-print(f"{ticker} is currently at: ${current_stock_price}")
-
-
-# Checking to see if the time is during open market hours
+current_date = date.today()
+market_close = dt.datetime(year=current_date.year, month=current_date.month, day=current_date.day, hour=16, minute=30)
 current_time = dt.datetime.now()
+market_hours = ""
+if current_time < market_close:
+    market_hours = True
+else:
+    market_hours = False
 
-# Market opens at 9:30
-# Open hour
-hour_open = 9
-# Open minute
-minute_open = 30
-# Market closes at 4:30
-# Close Hour 4
-hour_close = 4
-# Close minute
-minute_close = 30
+while market_hours:
+    r = requests.get(url, headers=headers)
 
+    html = r.text
+    soup = BeautifulSoup(html, "lxml")
 
-# Creating and reading a data.txt file to store the stocks past prices
-with open("data.txt", "r") as f:
-    count = int(f.readline().strip("counter:")) + 1
-    data = f.readlines()
-    f.close()
+    # Again, there has to be an easier way I simply have not found that way yet. Edit: Found
+    # I originally tried using "find" this way but I typed instead of copying the class... at least I know now
+    current_stock_price = soup.find("span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").text
+    print(f"{ticker} is currently at: ${current_stock_price}")
 
-# Writing to data.txt file updating count
-with open("data.txt", "w") as f:
-    f.write(f"counter:{count}\n")
-    f.close()
+    # Creating and reading a data.txt file to store the stocks past prices
+    with open("data.txt", "r") as f:
+        count = int(f.readline().strip("counter:")) + 1
+        data = f.readlines()
+        f.close()
 
-# Appending old information and adding new data at the end
-with open("data.txt", "a") as f:
-    for line in range(len(data)):
-        f.write(data[line])
-    f.write(f"Time:{current_time}\nStock:{ticker},Price:{current_stock_price}\n")
-    f.close()
+    with open("data.txt", "w") as f:
+        f.write(f"counter:{count}\n")
+        f.close()
 
+    with open("data.txt", "a") as f:
+        for line in range(len(data)):
+            f.write(data[line])
+        f.write(f"Time:{current_time}\nStock:{ticker},Price:{current_stock_price}\n")
+        f.close()
+    time.sleep(interval)
+    current_time = dt.datetime.now()
+    if current_time < market_close:
+        market_hours = True
+    else:
+        market_hours = False
 
-# TODO Send current stock price to excel sheet w/h time stamp
-# TODO Update current stock price every x period of time
 # TODO Modularize code
 # TODO Find a better way to access the current stock price
 # TODO Create line graph tracking stock price
